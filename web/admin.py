5731a5e1a1b3cad3003f596766cade06f0814142
Ricardo Ramirez <kike.rjramire@gmail.com>
2024-03-04 15:36:23 -0500

    registro de modelos en admin,uso de tailwind

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Producto
+admin.site.register(Categoria)
+# admin.site.register(Producto)
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'categoria')
