ec36f904467ff24431c9a602219ed62a79be64d9
Ricardo Ramirez <kike.rjramire@gmail.com>
2024-04-11 11:19:22 -0500

    django forms para registro y login de clientes

---
+++ web/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+# para que se muestre como un input de tipo date
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class ClienteForm(forms.Form):
+    SEXO_CHOICES = (('M', 'Masculino'), ('F', 'Femenino'),
+                    ('O', 'Otro')
+                    )  # tupla de tuplas
+
+    cedula = forms.CharField(label='Cedula', max_length=13)
+    nombre = forms.CharField(label='Nombre', max_length=200, required=True)
+    apellido = forms.CharField(label='Apellido', max_length=200, required=True)
+    sexo = forms.CharField(label='Sexo', max_length=1)
+    fecha_nacimiento = forms.DateField(
+        label='Fecha de Nacimiento', input_formats=['%Y-%m-%d'], widget=DateInput())
+    telefono = forms.CharField(label='Telefono', max_length=20)
+    # el widget es para que se muestre como un textarea
+    direccion = forms.CharField(label='Direccion', widget=forms.Textarea)
+    username = forms.CharField(label='Usuario', max_length=150)
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    email = forms.EmailField(label='Correo Electronico', max_length=150)
+    sexo = forms.ChoiceField(label='Sexo', choices=SEXO_CHOICES)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -21,3 +22,50 @@ class Producto (models.Model):
 
     def __str__(self):
         return self.nombre
+
+# relacion 1 a 1 con la tabla User de django
+
+
+class Cientes (models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.RESTRICT)
+    cedula = models.CharField(max_length=13)
+    sexo = models.CharField(max_length=1, default='M')
+    fecha_nacimiento = models.DateField()
+    telefono = models.CharField(max_length=20)
+    direccion = models.TextField()
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.usuario.username + " - " + self.cedula
+
+
+class Pedido(models.Model):
+
+    ESTADO_CHOICES = (
+        ('P', 'Pendiente'),
+        ('E', 'Enviado'),
+        ('C', 'Pagado'),
+    )
+
+    # relacion con la tabla clientes (1 a muchos) es decir un cliente puede tener muchos pedidos
+    # ForeignKey = llave foranea (relacion 1 a muchos)
+    cliente = models.ForeignKey(Cientes, on_delete=models.RESTRICT)
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    nro_pedido = models.CharField(max_length=20, null=True)
+    fecha_entrega = models.DateTimeField()
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    estado = models.CharField(
+        max_length=1, default='P', choices=ESTADO_CHOICES)
+
+    def __str__(self):
+        return self.cliente.usuario.username + " - " + str(self.fecha_pedido)
+
+
+class PedidoDetalle(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.RESTRICT)
+    producto = models.ForeignKey(Producto, on_delete=models.RESTRICT)
+    cantidad = models.IntegerField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.producto.nombre + " - " + str(self.cantidad)
